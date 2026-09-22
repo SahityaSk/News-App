236fddc8af3db4014498f24f8bba0239d35220a5
Arnab Das <darnab628@gmail.com>
2026-09-22 20:16:53 +0530

    database migration

--- migration-static-firebase/firebase-config.js
+++ migration-static-firebase/firebase-config.js
@@ -1,12 +1,13 @@
 // Firebase web configuration is safe to expose in a static browser app.
 // Security must be enforced with Firebase Authentication and database rules.
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBDoFoT2GZUEwjZkCA049ewHJPzIH2FQaI',
-  authDomain: 'news-app-2e28d.firebaseapp.com',
-  projectId: 'news-app-2e28d',
-  storageBucket: 'news-app-2e28d.firebasestorage.app',
-  messagingSenderId: '496725138189',
-  appId: '1:496725138189:web:0ac79a37d78f97c957ae44'
+  apiKey: 'AIzaSyDcIemy_D7BBVQ0KFva_qtqb_7E23_JgAM',
+  authDomain: 'yugantar-news.firebaseapp.com',
+  projectId: 'yugantar-news',
+  storageBucket: 'yugantar-news.firebasestorage.app',
+  messagingSenderId: '345131049402',
+  appId: '1:345131049402:web:a0776dbe37ee6434e67d83',
+  measurementId: 'G-LFRELT5SJK'
 };
 
 export const firebaseConfigured = true;
